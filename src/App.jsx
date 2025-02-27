e6f60af8558ae4da906e423299b2379e1f5dc26e
Riya Rastogi <riya02rastogi@gmail.com>
2025-02-27 07:51:22 +0530

    HomePage Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./Layout.scss"
 import Navbar from "./components/Navbar/Navbar";
+import HomePage from "./routes/homepage/homePage"
 function App() {
 
   return (
     <div className="Layout">
-       <Navbar/>
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="content">
+        <HomePage/>
+      </div>
+       
     </div>
   )
 }
---
+++ src/routes/homepage/homePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./homePage.scss"
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+          <div className="wrapper">
+             <h1 className="title">
+               Find real Estate & Get Your Dream Place
+              </h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel enim quam iure. Nisi saepe eum quo incidunt sint, autem provident ratione. Ea, doloribus. Eligendi assumenda exercitationem illum velit reiciendis obcaecati.</p>
+          </div>
+      </div>
+      <div className="imgContainer">
+          <img src="/bg.png" alt="" />
+      </div>
+      
+    </div>
+  )
+}
+
+export default HomePage
