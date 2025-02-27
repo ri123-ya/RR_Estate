6b5547b141b17ea4c05b74edfbbb0c067d40ea71
Riya Rastogi <riya02rastogi@gmail.com>
2025-02-27 14:28:39 +0530

    Homepage layout

--- src/routes/homepage/homePage.jsx
+++ src/routes/homepage/homePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./homePage.scss"
+import SearchBar from "../../components/searchBar/SearchBar"
 
 const HomePage = () => {
   return (
@@ -9,7 +10,25 @@ const HomePage = () => {
              <h1 className="title">
                Find real Estate & Get Your Dream Place
               </h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel enim quam iure. Nisi saepe eum quo incidunt sint, autem provident ratione. Ea, doloribus. Eligendi assumenda exercitationem illum velit reiciendis obcaecati.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel enim quam iure. Nisi saepe eum quo incidunt sint, autem provident ratione. Ea, doloribus. Eligendi assumenda exercitationem illum velit reiciendis obcaecati.
+              </p>
+
+              <SearchBar/>
+              <div className="boxes">
+                 <div className="box">
+                    <h1>16+</h1>
+                    <h2>Years of Experience</h2>
+                 </div>
+                 <div className="box">
+                    <h1>150</h1>
+                    <h2>Award Gained</h2>
+                 </div>
+                 <div className="box">
+                    <h1>1200+</h1>
+                    <h2>Property Ready</h2>
+                 </div>
+              </div>
           </div>
       </div>
       <div className="imgContainer">
